5d203109295c71a31b38e256896f67e1509703bb
Alexandr Dmitriev <alexandrdmtriev@gmail.com>
2022-11-24 18:01:13 +0600

    added calendar component and dates

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import calendarDate from '../assets/img/calendar-date.png';
+import caretUp from '../assets/img/caretUp.png';
+import {calendarItems} from '../constants';
 
 const Calendar = () => {
   return (
-    <div className='calendar'></div>
-  )
-}
+    <div className="calendar">
+      <div className="header">
+        <div className="title">
+          <img src={calendarDate} alt="calendar-date" />
+          <p>Календарь</p>
+        </div>
+      </div>
+      <div className="btns">
+        <span>
+          <img src={caretUp} alt="caret-up" />
+        </span>
+      </div>
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div className="item" key={item}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,1 @@
+export const calendarItems = ['сегодня', 'неделя', 'все']
