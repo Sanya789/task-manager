8ee935e6b10d99e1e4c243b2988f71029a9646c5
Alexandr Dmitriev <alexandrdmtriev@gmail.com>
2022-11-26 15:18:26 +0600

    added todo component witsh mock data

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import checkFill from '../assets/img/check-fill.png';
+import checkEmpty from '../assets/img/check-empty.png';
+import arrowClockwise from '../assets/img/arrowClockwise.png';
+import trash from '../assets/img/trash.png';
 
-const Todo = () => {
+const Todo = ({ todo }) => {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className='todo'>todo</div>
-  )
-}
+    <div className="Todo">
+      <div 
+      className="todo-container" 
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={()=> setHover(false)}
+      >
+        <div className="check-todo">
+          {todo.checked ? (
+            <span className="checked">
+              <img src={checkFill} alt="checked" />
+            </span>
+          ) : (
+            <span className="unchecked">
+              <img src={checkEmpty} alt="checked" />
+            </span>
+          )}
+        </div>
+        <div className="text">
+          <p style={{ color: todo.checked ? '#bebebe' : 'black' }}>{todo.text}</p>
+          <span>
+            {todo.time} - {todo.title}
+          </span>
+          <div className={`line ${todo.checked ? 'line-through' : ''}`}></div>
+        </div>
+        <div className="add-to-next-day">
+          {todo.checked && (
+            <span>
+              <img src={arrowClockwise} alt="arrow" />
+            </span>
+          )}
+        </div>
+        <div className="delete-todo">
+          {(hover || todo.checked) && (
+            <span>
+              <img src={trash} alt="trash" />
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import Todo from './Todo'
-import NextWeek from './NextWeek'
+import React from 'react';
+import Todo from './Todo';
+import NextWeek from './NextWeek';
 
 const Todos = () => {
-  return (
-    <div className='todos'>todos
-      <Todo/>
-      <NextWeek/>
-    </div>
-  )
-}
+  const selectedTitle = 'Сегодня';
+  const todos = [
+    {
+      id: '1',
+      text: 'test',
+      time: '10:00 AM',
+      date: '06/03/2022',
+      day: '6',
+      checked: true,
+      color: '#000000',
+      title: 'personal',
+    },
+    {
+      id: '2',
+      text: 'test 2',
+      time: '9:00 AM',
+      date: '08/03/2022',
+      day: '1',
+      checked: false,
+      color: '#00ff00',
+      title: 'work',
+    },
+  ];
 
-export default Todos
+  return (
+    <div className="Todos">
+      <div className="selected-title">{selectedTitle}</div>
+      <div className="todos">
+        {selectedTitle === 'Неделя' ? (
+          <NextWeek todos={todos} />
+        ) : (
+          todos.map((todo) => <Todo todo={todo} key={todo.id} />)
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
